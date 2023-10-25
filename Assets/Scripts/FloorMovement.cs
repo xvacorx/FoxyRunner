1076a561bf07918819ca463ba00605f5b503726c
xvacorx <theproofthevictors@gmail.com>
2023-10-25 11:10:56 -0300

    update

--- Assets/Scripts/FloorMovement.cs
+++ Assets/Scripts/FloorMovement.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class FloorMovement : MonoBehaviour
 {
-    public float offsetX = 21f;
-    public float speed = 5f;
+    public float offsetX = 22f;
+    public float ScrollSpeed = 6.0f;  // Velocidad inicial de desplazamiento
+    public float accelerationRate = 0.5f;    // Tasa de aceleración
+    private float timeElapsed = 0f;        // Tiempo transcurrido desde el inicio
 
-    private void Update()
+    bool active = true;
+
+    void Update()
     {
-        transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
-
-        if (transform.position.x <= -offsetX)
+        while (active)
         {
-            transform.position = new Vector3(offsetX, transform.position.y, transform.position.z);
+            // Aumenta el tiempo transcurrido
+            timeElapsed += Time.deltaTime;
+
+            // Calcula la velocidad de desplazamiento en función del tiempo transcurrido
+            float currentScrollSpeed = ScrollSpeed + accelerationRate * timeElapsed;
+
+            // Mueve el objeto con la velocidad calculada
+            transform.Translate(Vector2.left * currentScrollSpeed * Time.deltaTime);
+
+            if (transform.position.x <= -offsetX)
+            {
+                transform.position = new Vector3(offsetX, transform.position.y, transform.position.z);
+            }
         }
     }
 }
